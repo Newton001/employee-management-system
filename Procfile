7dddfc849c70ef8693bf8dcacd1de268901132f0
Newton001 <Newtonkelvin75@gmail.com>
2020-09-08 14:16:07 +0300

    Deploying to heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,4 @@
+release: python3.6 manage.py makemigrations --no-input
+release: python3.6 manage.py migrate --no-input
 web: gunicorn lemaproject.wsgi --log-file -
+
--- lemaproject/settings.py
+++ lemaproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import django_heroku
 from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -145,3 +145,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
